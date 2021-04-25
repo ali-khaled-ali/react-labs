04f50ebd44e0aa18286df375d8bb9b556ad9034f
ali-khaled-ali <alikhaledali1010@gmail.com>
2021-04-25 21:30:37 +0200

    json obj display/lab1

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,15 +5,45 @@ import { render } from 'react-dom';
 
 class App extends React.Component{
 
+  constructor(){
+    super();
+    this.state={
+      id:"1",
+      jobTitleName:"Developer",
+      firstName:"Ahmed",
+      lastName:"Ali",
+      preferredFullName:"Ahmed Ali",
+      employeeCode:"E1",
+      region:"CA",
+      phoneNumber:"408-1234567",
+      emailAddress:"ahmed.ali@gmail.com"
+    }
+  }
+
 render() {
   return (
     <div>
+    
+
+      <div>
+          <p>my name is: {this.state.preferredFullName}</p>
+          <p>i work as a: {this.state.jobTitleName}</p>
+          <p>my email is: {this.state.emailAddress}</p>
+          <p>my phone is: {this.state.phoneNumber}</p>
+        
+      </div>
+
+
       <Counter/>
     </div>
     
   );
 }
 }
+
+
+
+
 class Counter extends React.Component{
 
   constructor(){
